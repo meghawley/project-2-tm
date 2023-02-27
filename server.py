1d8561aa4c0c0c534f015f67cfb10aa0ad43a776
Max <maxno@instance-1-practice.c.folkloric-air-376518.internal>
2023-02-27 19:05:52 +0000

    2/27 edits

--- server.py
+++ server.py
@@ -1,30 +1,29 @@
-import grpc
+import grpc 
 import numstore_pb2
 import numstore_pb2_grpc
 import math
 from concurrent import futures
 
-server_dict = {}
-count = 0
+server_dict={}
+count=0
 
 class Server_imp(numstore_pb2_grpc.NumStoreServicer):
-    def SetNum(self, sn1, sn2):
-        server_dict[sn1.key] = sn1.value
-        count+= sn1.value - server_dict.get(sn1.key, None)
-        return numstore_pb2.Total(value = count)
+    def SetNum(self, sn1,sn2):
+        global count
+        server_dict[sn1.key]=sn1.value
+        count+=sn1.value - server_dict.get(sn1.key)
+        return numstore_pb2.SetNumResponse(total = count)
     def Fact(self, f1,f2):
-        value = server_dict.get(f1.key, None)
+        value = server_dict.get(f1.key)
         if value == None:
-            return numstore_pb2.NumResponse(error = "Not Found")
+            return numstore_pb2.FactResponse(error="Not Found")
         else:
-            return numstore_pb2.NumResponse(value = math.factorial(value))
+            return numstore_pb2.FactResponse(value = math.factorial(value))
 def server():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers = 4))
-    numstore_pb2_grpc.add_NumStoreServicer_to_server(NumStoreServicer(), server)
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=4))
+    numstore_pb2_grpc.add_NumStoreServicer_to_server(Server_imp(), server)
     server.add_insecure_port("[::]:5440")
     server.start()
     server.wait_for_termination()
-        
 if __name__ == '__main__':
-
     server()
